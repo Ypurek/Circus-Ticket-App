2a5f2430a863d43a23cce11055c7a864490eed32
Ypurek <oleksii.ostapov@gmail.com>
2017-12-19 14:41:11 +0200

    apache fix

--- circus/settings.py
+++ circus/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY = 'dfdsvdfvdfvmdfvmdpfe904iv9k456g54690b5406h78b07b8n50'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
