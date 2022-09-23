bf8298a4152d142d7141769861e7e0d32cf00d10
Ypurek <oleksii.ostapov@infopulse.com>
2022-09-23 13:21:25 +0300

    updated settings for secret and static files

--- circus/settings.py
+++ circus/settings.py
@@ -19,7 +19,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ob@y_v3no5+fcuybn2j0*d%rhkbt%ghl0iu8wu+36#1fjh%y_='
+key = os.getenv('SECRET_KEY')
+SECRET_KEY = key if key is not None else 'not secure key here'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -116,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     ('assets', os.path.join(BASE_DIR, "assets"))
 ]
